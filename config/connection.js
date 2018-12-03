7c65e68e8c3fc64971e9fe9b53f18c3e69a10874
chad <irsyad.muhd@gmail.com>
2018-12-04 01:29:12 +0800

    init project

---
+++ config/connection.js
@@ -0,0 +1,29 @@
+const development = {
+  database: 'illumatedb',
+  username: 'illumateuser',
+  password: 'ill$umate!',
+  host: 'localhost',
+  dialect: 'postgres',
+};
+
+const testing = {
+  database: 'databasename',
+  username: 'username',
+  password: 'password',
+  host: 'localhost',
+  dialect: 'sqlite' || 'mysql' || 'postgres',
+};
+
+const production = {
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST || 'localhost',
+  dialect: 'sqlite' || 'mysql' || 'postgres',
+};
+
+module.exports = {
+  development,
+  testing,
+  production,
+};
