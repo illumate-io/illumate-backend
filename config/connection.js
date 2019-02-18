d818778d26ba3138198e5c32ca2cc842819f5c54
chad <irsyad.muhd@gmail.com>
2019-02-19 01:17:06 +0800

    config env var

--- config/connection.js
+++ config/connection.js
@@ -17,7 +17,7 @@ const testing = {
 const production = {
   database: process.env.DB_NAME || 'd8a3m24u3qqk8v',
   username: process.env.DB_USER || 'hxndqobettwyyk',
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASS || '8708afba8019a0d85cbef3aa9508b66e4d14b76f888b8f5ca7cc4f45108f756e',
   host: process.env.DATABASE_URL || 'localhost',
   dialect: 'postgres',
 };
