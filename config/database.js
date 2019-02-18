0ed0a4cfd4b6def2944591e27f584a4e3e0d1ec8
chad <irsyad.muhd@gmail.com>
2019-02-19 01:50:42 +0800

    add null

--- config/database.js
+++ config/database.js
@@ -8,7 +8,12 @@ let database;
 switch (process.env.NODE_ENV) {
   case 'production':
     console.log(connection.production);
-    database = new Sequelize(connection.production.database, { dialect: 'postgres' });
+    database = new Sequelize(
+      connection.production.database,
+      null,
+      null,
+      { dialect: 'postgres' },
+    );
     //   // connection.production.database,
     //   // connection.production.username,
     //   // connection.production.password, {
